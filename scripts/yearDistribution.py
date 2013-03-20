adea70c32a5956de85c62f8bd7efaa3b211e72ef
vsubhashini <subhashini.venugopalan@gmail.com>
2013-03-19 19:47:52 -0500

    Year-wise distribution script added.

---
+++ scripts/yearDistribution.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# Get top 3 topics for each abstract from mallet output (doc-composition)
+# Write the corresponding technology areas (and classification numbers)
+__author__="vsubhashini"
+
+import csv
+import re
+
+def import_text(filename,separator):
+  for line in csv.reader(open(filename), delimiter=separator, skipinitialspace=True):
+    if line:
+      yield line
+
+def getYearDistribution(patentFile, outfile):
+  count=0;
+  ofid = open(outfile, 'w+')
+  yearset = set()
+  yearCount={}
+  for line in import_text(patentFile,','):
+    if count==0:
+      count+=1
+      continue
+    year = line[1]
+    yearset.add(year)
+    ycount = yearCount.get(year)
+    if ycount==None:
+      ycount=0
+    ycount+=1
+    yearCount[year]=ycount
+  total=0
+  for year in yearCount:
+    total+=yearCount[year]
+    print year, yearCount[year]
+  print "Total: " + str(total)
+
+def main():
+  patentFile="../data/Solar_Innovation_Database_PRP.csv";
+  outputFile="../data/pyAnalysisOutput/yearDistribution.txt";
+  getYearDistribution(patentFile, outputFile)
+
+if __name__=="__main__":
+    main()
+  
